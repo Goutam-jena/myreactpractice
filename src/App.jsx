5469b99afa8a97af4b9a43abdd53e0a6c265b16e
Goutam jena <goutamjena023@gmail.com>
2026-05-18 06:28:02 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Hello World</h1>
+      <p>My clean React scratchpad is ready.</p>
+    </div>
+  );
+}
+
+export default App;
